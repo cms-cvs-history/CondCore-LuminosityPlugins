63a549eb8a21c40211a31410cb928c0f66752df6
cmsbuild <>
2010-02-25 00:29:44 +0000

    This commit was manufactured by cvs2git to create tag 'CondCore- LuminosityPlugins-CMSSW_3_8_0'.
    Sprout from master 2009-12-14 13:03:50 UTC Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch> 'first migration'
    Cherrypick from master 2010-02-25 00:29:11 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'convert to current macros from deprecated ones':
        src/plugin.cc

--- src/plugin.cc
+++ src/plugin.cc
@@ -1,8 +1,8 @@
-#include "CondCore/PluginSystem/interface/registration_macros.h"
+#include "CondCore/ESSources/interface/registration_macros.h"
 #include "CondFormats/Luminosity/interface/LumiSectionData.h"
 #include "CondFormats/DataRecord/interface/LumiSectionDataRcd.h"
 
-DEFINE_SEAL_MODULE();
+
 REGISTER_PLUGIN(LumiSectionDataRcd, lumi::LumiSectionData);
 
 
